d404acfc99b73b4d86f14708d7c1242134772437
alexander-iov <sasha.iovsa@gmail.com>
2021-03-05 18:45:51 +0200

    delete non-used code

--- src/main/java/task2/MyServlet.java
+++ src/main/java/task2/MyServlet.java
@@ -3,12 +3,11 @@ package task2;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Iterator;
 
 public class MyServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         String blockedQueryParam = "Stanislav";
         Iterator<String> stringIterator = req.getParameterNames().asIterator();
         while (stringIterator.hasNext()) {
