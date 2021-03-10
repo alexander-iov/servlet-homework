bf19d7b851d2a827b9b1d4c5e74bd695f27d3777
alexander-iov <sasha.iovsa@gmail.com>
2021-03-10 20:54:07 +0200

    refactoring final

--- src/main/java/task2/MyServlet.java
+++ src/main/java/task2/MyServlet.java
@@ -1,17 +1,11 @@
 package task2;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Iterator;
 
 public class MyServlet extends HttpServlet {
-
-    @Override
-    public void init() throws ServletException {
-        super.init();
-    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
