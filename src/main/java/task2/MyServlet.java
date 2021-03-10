b918db4c8584f8146b8dae62e2882e447221f272
alexander-iov <sasha.iovsa@gmail.com>
2021-03-10 20:42:39 +0200

    refactoring

--- src/main/java/task2/MyServlet.java
+++ src/main/java/task2/MyServlet.java
@@ -1,21 +1,26 @@
 package task2;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Iterator;
 
 public class MyServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        String blockedQueryParam = "Stanislav";
         Iterator<String> stringIterator = req.getParameterNames().asIterator();
         while (stringIterator.hasNext()) {
             String next = stringIterator.next();
-            if (next.equals(blockedQueryParam)) {
+            if (next.equals(getServletConfig().getInitParameter("blockedQueryParam"))) {
                 resp.setStatus(400);
-            } else {
-                resp.setStatus(200);
+                return;
             }
         }
     }
